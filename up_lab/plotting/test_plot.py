39532ef01422b50054c8fa67591a76d3a88b9475
AlexaEicker <alexa.eicker@eisystems.de>
2024-03-30 16:42:24 +0100

    wrote test function
    Wrote tuple_return_test to understand it better

---
+++ up_lab/plotting/test_plot.py
@@ -0,0 +1,24 @@
+#from up_lab.plotting.plot import array_plot
+import matplotlib.pyplot as plt
+import numpy as np
+#Example data
+xdata = [123,345,340,346,645,234,356]
+ydata = [2,4,5,9,3,6,3]
+def linfunc(x,a,b):
+    return a*x+b
+#Test array_plot
+#array_plot(xdata, ydata,yerr=0.4,weighted=True)
+
+plt.errorbar(xdata,ydata, yerr=0.3,)
+plt.show()
+
+
+
+
+def tuple_return_test(a,b,c):
+    returnval = [a,b,c]
+    return *returnval,
+one = np.array([1])
+a,b,c = tuple_return_test(one,True,'Love')
+print(a,b)
+print(type())
