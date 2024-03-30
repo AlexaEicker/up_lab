c09193301628a942dfd9e8afafcd07478e6f61f7
AlexaEicker <alexa.eicker@eisystems.de>
2024-03-30 16:22:16 +0100

    Changes of classifiers and setup
    According to pypi categories. Added github link and changed readme to new readme.md file

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 from setuptools import setup, find_packages
 
 classifiers =[
-	'Development Status :: 5 - Production/Stable',
-	'Intended Audience :: Higher Education',
+	'Development Status :: 2 - Pre-Alpha',
+	'Intended Audience :: Education',
 	'Operating system :: Microsoft :: Windows :: Windows 10',
 	'Licence :: MIT Licence',
-	'Programming Language :: Python :: 3']
+	'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering :: Physics']
 
 setup(
 	name='up_lab',
 	version = '0.0.1',
 	description='Collection of useful commands and analysing functions for the physics laboratory at University of Potsdam, Germany',
-	long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-	url='',
+	long_description=open('README.md','r',encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+	url='https://github.com/AlexaEicker/up_lab',
 	author='Alexa Eicker',
 	classifiers=classifiers,
-	keywords=['laboratory','analyse','plot','python', 'Germany','Potsdam','university'],
+	keywords=['laboratory','analyse','plot','python', 'Germany','Potsdam','experiment','experimental physics'],
 	packages=find_packages(),
 	install_requires=['matplotlib', 'numpy', 'scipy', 'pandas', 'sympy']
 )
+
+#TODO: check wheter long_description works
