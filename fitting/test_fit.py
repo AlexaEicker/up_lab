3d7246592f8773b12b13a247fa588590833afc5d
AlexaEicker <alexa.eicker@eisystems.de>
2024-03-30 16:25:05 +0100

    Created new root folder
    Moved all files to new subfolders of up_lab_package folder

--- fitting/test_fit.py
+++
@@ -1,9 +0,0 @@
-#from basic_functions import linear_fit
-from fitting.fit import least_square_method_fit
-import matplotlib.pyplot as plt
-
-
-X = [1, 2, 3, 4, 5]
-Y = [2, 3, 5, 4, 6]
-
-#print(least_square_method_fit(X,Y,weighted=True))
--- plotting/transform.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-
-#xdata = pd.read_csv()
