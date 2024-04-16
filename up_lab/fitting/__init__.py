1278eac0a6bc67088273fe76ebf96c40296312e7
AlexaEicker <alexa.eicker@eisystems.de>
2024-04-17 00:39:18 +0200

    Update init files
    Added all functions to import statement, slight changes in all variable

--- up_lab/fitting/__init__.py
+++ up_lab/fitting/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['basic_functions','fit']
+__all__ = ['fit']
 
-from basic_functions import linear_fit,quadratic_function,cubic_function,quartic_function,exponential_function,logarithmic_function,sigmoid_function,power_law_function,gaussian_function, sine_function, cosine_function, tangent_function, function_availibility
-from fit import least_square_method_fit, chi_square, reduced_chi_square
+
+from fit import linear_fit,quadratic_function,cubic_function,quartic_function,exponential_function,logarithmic_function,sigmoid_function,power_law_function,gaussian_function, sine_function, cosine_function, tangent_function, function_availibility,fit_ls, chi_square, reduced_chi_square
--- up_lab/plotting/__init__.py
+++ up_lab/plotting/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['plot','transform']
+from plot import array_plot
