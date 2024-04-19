b39241d4b61718731b308a34f731a8b711825f03
AlexaEicker <alexa.eicker@eisystems.de>
2024-04-19 19:32:30 +0200

    Minimal changes
    Added import fitting, not sure wheter thats necessary and adjusted function name

--- up_lab/calculate/__init__.py
+++ up_lab/calculate/__init__.py
@@ -1,5 +1,5 @@
 from basic_calc import type_change, statistical_analysis, hypothesis_test, least_squares_regression
-from uncertainty import type_a,type_b,device_acuracy, combined_unc,expanded_unc,error_propagation, uncertainty
+from uncertainty import type_a,type_b,device_acuracy, combined_unc,expanded_unc,error_propagation, totuncertainty
 from conversion import K_to_C,C_to_K,F_to_K, in_to_m, ft_to_m,mi_to_m, oz_to_kg, lb_to_kg, min_to_sec,sec_to_min, h_to_min, min_to_h, sec_to_h, h_to_sec
-
+import fitting
 __all__ = ['basis_calc', 'conversion','uncertainty']
